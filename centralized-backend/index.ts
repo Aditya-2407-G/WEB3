6266368a5f95413bbe968367e65f8a298ee96f18
Aditya <92910224+Aditya-2407-G@users.noreply.github.com>
2024-05-04 12:15:08 +0530

    Update index.ts

--- centralized-backend/index.ts
+++ centralized-backend/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { abi } from "./abi";
 
 const providerUrl = "wss://eth-goerli.g.alchemy.com/v2/u4LDb5PsjbhInoG7S5LHLHzYe85f2pU1";
-const contractAddress = "0x83cdb6ed4915be70805afb3dfeba6a1a677d1bfb";
+const contractAddress = "0x83cdb6ed4915be70805afb3dfeba6a1a677d1bfb";   // RPC URL  nodes miners to connect to 
 const webhookUrl = "https://your-dummy-fetch-url";
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider(providerUrl));
