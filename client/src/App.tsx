7329f4feddba9e7251de432e90cab47e74f4cd5b
Aditya <92910224+Aditya-2407-G@users.noreply.github.com>
2024-05-08 03:43:24 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,50 +1,49 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {contractABI} from "./abi";
-import {Admin} from "./Admin";
-import {CourseRegistration} from "./CourseRegistration";
+import { contractABI } from "./abi";
+import { Admin } from "./Admin";
+import { FundMeRegistration } from "./FundMeRegistration";
+import Web3 from 'web3';
 
 function App() {
-    const [web3, setWeb3] = useState(null);
-    const [courseContract, setCourseContract] = useState(null);
-    const [courseFee, setCourseFee] = useState('');
-    const contractAddress = '0x1a1F655c9f3da5384A1cDB6C68c10F4B62D89f11';
+    const [web3, setWeb3] = useState<Web3 | null>(null);
+    const [fundMeContract, setFundMeContract] = useState<any>(null);
+    const [fundFee, setFundFee] = useState(null);
+    const contractAddress = "0x66E5367B422977338C6088D21C7577e907176693"
 
     useEffect(() => {
         if (window.ethereum) {
             window.ethereum.request({ method: 'eth_requestAccounts' })
                 .then(() => {
-                    //@ts-ignore
                     const web3Instance = new Web3(window.ethereum);
                     setWeb3(web3Instance);
 
-                    const courseInstance = new web3Instance.eth.Contract(contractABI, contractAddress);
-                    setCourseContract(courseInstance);
+                    const fundMeInstance = new web3Instance.eth.Contract(contractABI, contractAddress);
+                    setFundMeContract(fundMeInstance);
 
-                    courseInstance.methods.courseFee().call()
-                    //@ts-ignore
-                        .then(fee => {
-                            setCourseFee(web3Instance.utils.fromWei(fee, 'ether'));
+                    fundMeInstance.methods.courseFee().call()
+                    .then(fee => {
+                            //@ts-ignore
+                            setFundFee(web3Instance.utils.fromWei(fee, 'ether'));
                         });
                 })
                 .catch((err: any) => {
-                    // Handle error if the user rejects the connection request
                     console.error(err);
                 });
         } else {
-            alert('Please install an another Ethereum wallet.');
+            alert('Please install another Ethereum wallet.');
         }
     }, []);
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<CourseRegistration web3={web3} courseContract={courseContract} courseFee={courseFee} />} />
-                <Route path="admin" element={<Admin web3={web3} courseContract={courseContract} courseFee={courseFee} />} />
+                <Route index element={<FundMeRegistration web3={web3} fundMeContract={fundMeContract} fundFee = {fundFee} />} />
+                <Route path="admin" element={<Admin web3={web3} fundMeContract={fundMeContract} />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
